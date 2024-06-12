4d50be5a156ca806bc4d5700afb5ab79b67da270
mayank20011 <mayank.behl.mb1@gmail.com>
2024-06-12 13:43:59 +0530

    updating readme for import exort and adding import.js, export.js file to learn variable and function access outside of module

---
+++ 2_export.js
@@ -0,0 +1,8 @@
+// lets create sum and different function here and the export them so that any file can use them
+
+export function sum(a = 0, b = 0) {
+  console.log(`Sum of a and b is ${a + b}`);
+}
+export function difference(a = 0, b = 0) {
+  console.log(`Diffrence of a and b is ${a - b}`);
+}
---
+++ 3_import.js
@@ -0,0 +1,4 @@
+// lets import sum and diffrence from 2_export.js file
+import { sum, difference } from "./2_export.js";
+sum(10, 20);
+difference(50, 10);
